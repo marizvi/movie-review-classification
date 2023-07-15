7f011f5156fc12490eaf0099c054034367290ff6
marizvi <202051121@iiitvadodara.ac.in>
2023-07-16 02:59:18 +0530

    vercel

---
+++ api/app.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import joblib
+from flask import Flask, jsonify, request
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    # Get Json request    
+    feat_data = request.get_json()
+    print(feat_data)
+    #convert json to dataframe
+    df = pd.DataFrame(feat_data)
+    df = df.reindex(columns=['phrase'])
+    print(df)
+
+    #predict
+    prediction = list(model.predict(df['phrase'].values))
+
+    return jsonify({'sentiment': str(prediction)})
+
+stop_words = set(stopwords.words('english'))
+
+def custom_tokenizer(text):
+# Tokenization
+    tokens = text.split()
+    
+    # Remove stopwords, and stem the words
+    processed_tokens = [
+        stemmer.stem(token)
+        for token in tokens
+        if token.lower() not in stop_words
+    ]
+    
+    # Return the processed tokens
+    return processed_tokens
+if __name__=='__main__':
+    stemmer = PorterStemmer()
+    model = joblib.load('movie_review_model_svc.pkl')
+    
+    app.run(port=5000, debug=True)
