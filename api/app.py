0eb53e0eff07fa390cedc21d88f2640aa30ca695
marizvi <202051121@iiitvadodara.ac.in>
2023-07-16 13:53:00 +0530

    grid

--- api/app.py
+++ api/app.py
@@ -34,7 +34,7 @@ def hello_world():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get Json request    
-    model = joblib.load('./api/pipe.pkl')
+    model = joblib.load('pipe.pkl')
 
     feat_data = request.get_json()
     print(feat_data)
