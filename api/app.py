1c3c803c7a7329d8807774f19ec9500310ee3b2b
marizvi <202051121@iiitvadodara.ac.in>
2023-07-16 12:51:47 +0530

    grid

--- api/app.py
+++ api/app.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import joblib
 from flask import Flask, jsonify, request
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
+nltk.download('stopwords')
 
-# def custom_tokenizer(text):
-# # Tokenization
-#     stemmer = PorterStemmer()
-#     stop_words = set(stopwords.words('english'))
+def custom_tokenizer(text):
+# Tokenization
+    stemmer = PorterStemmer()
+    stop_words = set(stopwords.words('english'))
 
-#     tokens = text.split()
+    tokens = text.split()
     
-#     # Remove stopwords, and stem the words
-#     processed_tokens = [
-#         stemmer.stem(token)
-#         for token in tokens
-#         if token.lower() not in stop_words
-#     ]
+    # Remove stopwords, and stem the words
+    processed_tokens = [
+        stemmer.stem(token)
+        for token in tokens
+        if token.lower() not in stop_words
+    ]
     
-#     # Return the processed tokens
-#     return processed_tokens
+    # Return the processed tokens
+    return processed_tokens
 
 app = Flask(__name__)
-# model = joblib.load('static/grid.pkl')
+model = joblib.load('static/grid.pkl')
 
 @app.route("/")
 def hello_world():
