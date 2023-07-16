6fd194c598fd97c5763603dc4b2a163d0a77b288
marizvi <202051121@iiitvadodara.ac.in>
2023-07-16 16:42:03 +0530

    deplo1

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
 import pandas as pd
 import joblib
 from flask import Flask, jsonify, request
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
-# nltk.download('stopwords')
-
-# def custom_tokenizer(text):
-# # Tokenization
-#     stemmer = PorterStemmer()
-#     stop_words = set(stopwords.words('english'))
-
-#     tokens = text.split()
-    
-#     # Remove stopwords, and stem the words
-#     processed_tokens = [
-#         stemmer.stem(token)
-#         for token in tokens
-#         if token.lower() not in stop_words
-#     ]
-    
-#     # Return the processed tokens
-#     return processed_tokens
 
 app = Flask(__name__)
 
@@ -33,7 +11,7 @@ def hello_world():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get Json request    
-    model = joblib.load('gird_model.pkl')
+    model = joblib.load('grid_model.pkl')
 
     feat_data = request.get_json()
     print(feat_data)
@@ -41,8 +19,6 @@ def predict():
     df = pd.DataFrame(feat_data)
     df = df.reindex(columns=['phrase'])
     print(df)
-
-    # print('transform: ',vect.fit_transform(df['phrase'].values))
 
     #predict
     prediction = list(model.predict(df['phrase'].values))
